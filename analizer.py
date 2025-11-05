dd4a1f9b20ddf3d7e7ced8f0e41aec5d99dcf018
Nic <nicolasnaran@gmail.com>
2025-11-05 08:08:21 -0500

    feat: update class

--- analizer.py
+++ analizer.py
@@ -1,2 +1,17 @@
+import pandas as pd
+
+
+# TODO: add... everything????
 class Analizer:
-    pass
+    def __init__(self, data: pd.DataFrame) -> None:
+        self.data: pd.DataFrame = data
+
+    @classmethod
+    def from_csv(cls, file: str) -> "Analizer":
+        return cls(pd.read_csv(file))  # pyright: ignore[reportUnknownMemberType]
+
+
+if __name__ == "__main__":
+    test = Analizer.from_csv("./test_data.csv")
+
+    print(test.data.mean())
